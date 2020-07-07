1f61fa3e6ab39102115ff56800c74af374b27220
ZengQ <2254740843@qq.com>
2020-07-07 23:39:33 +0800

    初始化

---
+++ tk-mybatis-generator/src/main/java/com/collega/common/mapper/CollegeMapper.java
@@ -0,0 +1,7 @@
+package com.collega.common.mapper;
+
+import com.collega.common.entity.College;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface CollegeMapper extends Mapper<College> {
+}
---
+++ tk-mybatis-generator/src/main/java/com/collega/common/mapper/MajorMapper.java
@@ -0,0 +1,7 @@
+package com.collega.common.mapper;
+
+import com.collega.common.entity.Major;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface MajorMapper extends Mapper<Major> {
+}
